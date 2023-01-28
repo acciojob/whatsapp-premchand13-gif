4c77268ed3daeca1ad0c5451525aadb4d7c6872e
premchand13-gif <premechand1301@gmail.com>
2023-01-28 17:30:55 +0530

    modification-1

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -26,8 +26,12 @@ public class WhatsappController {
     public String createUser(String name, String mobile) throws Exception {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
+        String s=whatsappService.createUser(name, mobile);
+        if(s==null){
+            throw new Exception("User already exists");
+        }
 
-        return whatsappService.createUser(name, mobile);
+        return s;
     }
 
     @PostMapping("/add-group")
@@ -40,7 +44,7 @@ public class WhatsappController {
 
         //For example: Consider userList1 = {Alex, Bob, Charlie}, userList2 = {Dan, Evan}, userList3 = {Felix, Graham, Hugh}.
         //If createGroup is called for these userLists in the same order, their group names would be "Group 1", "Evan", and "Group 2" respectively.
-
+//some point miss(unique names in a group)
         return whatsappService.createGroup(users);
     }
 
@@ -57,8 +61,14 @@ public class WhatsappController {
         //Throw "Group does not exist" if the mentioned group does not exist
         //Throw "You are not allowed to send message" if the sender is not a member of the group
         //If the message is sent successfully, return the final number of messages in that group.
-
-        return whatsappService.sendMessage(message, sender, group);
+        int x=whatsappService.sendMessage(message, sender, group);
+        if(x==-1){
+            throw new Exception("Group does not exist");
+        }
+        else if(x==-2){
+            throw new Exception("You are not allowed to send message");
+        }
+        return x;
     }
     @PutMapping("/change-admin")
     public String changeAdmin(User approver, User user, Group group) throws Exception{
@@ -66,8 +76,17 @@ public class WhatsappController {
         //Throw "Approver does not have rights" if the approver is not the current admin of the group
         //Throw "User is not a participant" if the user is not a part of the group
         //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
-
-        return whatsappService.changeAdmin(approver, user, group);
+         String x=whatsappService.changeAdmin(approver, user, group);
+         if(x.equals("1")){
+             throw new Exception("Group does not exist");
+         }
+        if(x.equals("2")){
+            throw new Exception("Approver does not have rights");
+        }
+        if(x.equals("3")){
+            throw new Exception("User is not a participant");
+        }
+        return x;
     }
 
     @DeleteMapping("/remove-user")
@@ -78,8 +97,8 @@ public class WhatsappController {
         //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
         //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
         //If user is removed successfully, return (the updated number of users in the group + the updated number of messages in group + the updated number of overall messages)
-
-        return whatsappService.removeUser(user);
+       //whatsappService.removeUser(user);
+        return 1;
     }
 
     @GetMapping("/find-messages")
@@ -87,7 +106,7 @@ public class WhatsappController {
         //This is a bonus problem and does not contains any marks
         // Find the Kth latest message between start and end (excluding start and end)
         // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
-
-        return whatsappService.findMessage(start, end, K);
+         //whatsappService.findMessage(start, end, K);
+        return "hi";
     }
 }
--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -26,4 +26,98 @@ public class WhatsappRepository {
         this.customGroupCount = 0;
         this.messageId = 0;
     }
+    public String createUser(String name, String mobile){
+        if(userMobile.contains(mobile)){
+            return null;
+        }
+        userMobile.add(mobile);
+        return "SUCCESS";
+    }
+    public Group createGroup(List<User> users){
+        int c=users.size();
+        if(c==2){
+            Group group=new Group(users.get(1).getName(),c);
+            adminMap.put(group,users.get(0));
+            groupUserMap.put(group,users);
+            return group;
+        }
+        else if(c>2){
+            customGroupCount++;
+            Group group=new Group("Group "+customGroupCount,c);
+            adminMap.put(group,users.get(0));
+            groupUserMap.put(group,users);
+            return group;
+        }
+        return null;
+    }
+    //3
+    public int createMessage(String content){
+        messageId++;
+        Message message=new Message(messageId,content,new Date());
+        return messageId;
+
+    }
+    //4
+    public int sendMessage(Message message, User sender, Group group){
+        if(!groupUserMap.containsKey(group)){
+            return -1;
+        }
+        List<User> lst=groupUserMap.get(group);
+        if(!lst.contains(sender)){
+            return -2;
+        }
+        if(groupMessageMap.containsKey(group)){
+            List<Message> lt=groupMessageMap.get(group);
+            lt.add(message);
+            groupMessageMap.put(group,lt);
+        }
+        else{
+            List<Message> lt=new ArrayList<>();
+            lt.add(message);
+            groupMessageMap.put(group,lt);
+        }
+        return groupMessageMap.get(group).size();
+    }
+
+    //5
+    public String changeAdmin(User approver, User user, Group group){
+        if(!groupUserMap.containsKey(group)){
+            return "1";
+        }
+        List<User> lst=groupUserMap.get(group);
+
+        User admin=adminMap.get(group);
+        if(!admin.equals(approver)){
+            return "2";
+        }
+        if(!lst.contains(user)){
+            return "3";
+        }
+        adminMap.put(group,user);
+        return "SUCCESS";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
